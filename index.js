b8e12f96e4cac1ee32a1532fb19252d6d5708f53
Sanjin Brajević <mynamesafeboot@gmail.com>
2024-11-29 22:34:29 +0100

    More UI improvements

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+// ==UserScript==
+// @name        New script fit.ba
+// @namespace   Violentmonkey Scripts
+// @match       https://www.fit.ba/student/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 11/27/2024, 7:27:48 PM
+// ==/UserScript==
 function addViewportMeta() {
     const item = document.createElement('meta');
     item.name = 'viewport';
     item.content = 'width=device-width, initial-scale=1';
-
+  
     document.head.appendChild(item);
-}
-
-addViewportMeta();
+  }
+  
+  function fixFormHead() {
+    if (!window.location.pathname.startsWith('/student/nastava/dokumenti')) {
+      return;
+    }
+  
+    const element = document.querySelectorAll('.form-head')[1];
+  
+    if (!element) {
+      return;
+    }
+  
+    element.classList.remove('form-head');
+    element.classList.add('form-head-fixed');
+  }
+  
+  function lol() {
+    const random = Math.floor(Math.random() * 100000);
+  
+    if (random === 69420) {
+      alert('Čestitamo! Molimo vas pokupite ispisnicu u studnetskoj.');
+    }
+  }
+  
+  addViewportMeta();
+  fixFormHead();
+  lol();
